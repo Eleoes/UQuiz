686c5afdd0a1a12ab9e9474f9a668c8cc35f94d9
eleoes <leo.escaroz@gmail.com>
2022-08-27 15:18:13 -0700

    wip: Home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-page-container'>
+      <h1 className='home-title'>
+        <Link to="/create+quiz">Start</Link>
+      </h1>
+
+      <p className='description'>Do UQuiz</p>
+    </div>
   )
 }
 
