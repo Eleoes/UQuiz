6afff6fca16563658262153cfb1d5403f5a4af17
eleoes <leo.escaroz@gmail.com>
2022-08-27 15:37:16 -0700

    wip: Error page

--- src/components/pages/Error/Error.js
+++ src/components/pages/Error/Error.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import "./Error.css";
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className='error-container'>
+      <h1 className='error-title'>Page Not Found</h1>
+      <h3 className='error-title'>Please report this to: pending@email.com</h3>
+
+      <Link to='/' className='return-home'>Return Home</Link>
+    </div>
   )
 }
 
