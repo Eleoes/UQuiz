7b20a4ff9ab581fc44a34c2be150b814b06ed577
eleoes <leo.escaroz@gmail.com>
2022-08-30 22:47:12 -0700

    wip: modal, css

--- src/components/pages/QuizPage/Modal.js
+++ src/components/pages/QuizPage/Modal.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 import "./Modal.css";
 
-const Modal = ({closeModal}) => {
-
+const Modal = ({ closeModal, quizData }) => {
   return (
     <div className='modal-bg'>
         <div className='modal-container'>
@@ -11,14 +10,36 @@ const Modal = ({closeModal}) => {
                 <button onClick={() => closeModal(false)}>X</button>
             </div>
             <div className='modal-title'>
-                <h1>Question name</h1>
+                <h1>{quizData.name}</h1>
             </div>
             <div className='modal-body'>
-                <p>Keep going!</p>
+                {quizData.questions.map((item, index) => (
+                    <div className='question-container' key={index}>
+                        <h3 className='question'>
+                            {index + 1}. {item.question}
+                        </h3>
+                    
+                        <ul className='options-container'>
+                            {item.options.map((option, i) => (
+                                <li key={i} className='option'>
+                                    <input
+                                        type='radio'
+                                        name={item.question + index}
+                                        id={option + i + index}
+                                        onClick={() => {
+                                            item.userAnswer = i + 1;
+                                        }}
+                                    />{" "}
+                                    <label htmlFor={option + i + index}>{option}</label>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
             </div>
             <div className='modal-footer'>
                 <button onClick={() => closeModal(false)}>Cancel</button>
-                <button>Continue</button>
+                <button>Submit</button>
             </div>
         </div>
     </div>
